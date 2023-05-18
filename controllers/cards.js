e88b61b7e71f16eaec2bb686e08547c380229d14
Lyubov Trushnikova <lovelly2008@ya.ru>
2023-05-19 03:03:52 +0500

    Initial commit 13 sprint проверка9

--- controllers/cards.js
+++ controllers/cards.js
@@ -69,10 +69,11 @@ module.exports.dislikeCard = (req, res, next) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    // eslint-disable-next-line no-undef
-    .orFail(new NotFoundError('Карточка не найдена'))
     .then((card) => {
-      res.send({ data: card });
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.send({ data: card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
