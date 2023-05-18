5c7c665e616dfb56cac93872c394d81ce1027991
Lyubov Trushnikova <lovelly2008@ya.ru>
2023-05-18 16:10:12 +0500

    Initial commit 13 sprint проверка

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 //Здесь мы подключаем необходимые модули: Express для создания сервера, Mongoose для работы с MongoDB
 const express = require("express");
 const mongoose = require("mongoose");
+//пакет celebrate
+ const { errors } = require('celebrate');
 //Здесь мы подключаем модуль router, который содержит определение всех маршрутов для нашего приложения.
 const router = require("./routes/users");
 const routerCard = require("./routes/cards");
@@ -10,6 +12,9 @@ const { PORT = 3000 } = process.env;
 //Здесь мы создаем экземпляр приложения Express и настраиваем middleware для обработки JSON-данных.
 const app = express();
 app.use(express.json());
+
+// celebrate error handler
+app.use(errors());
 
 // подключаемся к серверу mongo
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
