09354c273a9cdc45bd9ebdfe5d6cdafdc0279c4c
Lyubov Trushnikova <lovelly2008@ya.ru>
2023-05-19 21:15:14 +0500

    Initial commit 13 sprint проверка9

--- app.js
+++ app.js
@@ -31,6 +31,17 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500 ? 'На сервере произошла ошибка.' : message,
+    });
+
+  next();
+});
 app.use('/', router);
 app.use('/cards', routerCard);
 
