2cc04e38f83cb6947c81171370361aa5e4546c02
Lyubov Trushnikova <lovelly2008@ya.ru>
2023-05-19 21:46:41 +0500

    Initial commit 13 sprint проверка9

--- app.js
+++ app.js
@@ -33,15 +33,11 @@ app.use((req, res, next) => {
 
 app.use('/', router);
 app.use('/cards', routerCard);
-// Обработчик ошибок
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500 ? 'На сервере произошла ошибка.' : message,
-    });
-  next();
+// Обработчик маршрута для неправильных путей
+app.use((req, res, next) => {
+  const error = new Error('Страница не найдена');
+  error.status = 404;
+  next(error);
 });
+
 app.listen(PORT);
