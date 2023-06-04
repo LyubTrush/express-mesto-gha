6224c506f8ce6d4b81400bfb06690c08bad92ee2
Lyubov Trushnikova <lovelly2008@ya.ru>
2023-06-04 15:04:48 +0500

    fix: errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 module.exports = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
+  const statusCode = err.status || 500;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
   next();
